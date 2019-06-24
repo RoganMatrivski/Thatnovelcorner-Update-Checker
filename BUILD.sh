5e388a54e02cb0341fa96a87f852d41dcf3b4c01
Rogan Matrivski <robinmauritzrm@gmail.com>
2019-06-25 06:05:01 +0700

    Fix build command

--- BUILD.sh
+++ BUILD.sh
@@ -1,6 +1,6 @@
-pkg -t node12-linux-x64 --out-path ./build/linux
-cp -vr ./node_modules/open/xdg-open ./build/linux
-pkg -t node12-macos-x64 --out-path ./build/macos
-cp -vr ./node_modules/open/xdg-open ./build/macos
-pkg -t node12-win-x32 --out-path ./build/win
-cp -vr ./node_modules/open/xdg-open ./build/win
+pkg . -t node12-linux-x64 --out-path ./build/linux;
+cp . -vr ./node_modules/opn/xdg-open ./build/linux;
+pkg . -t node12-macos-x64 --out-path ./build/macos;
+cp   . -vr ./node_modules/opn/xdg-open ./build/macos;
+pkg . -t node12-win-x32 --out-path ./build/win;
+cp -vr ./node_modules/opn/xdg-open ./build/win;
